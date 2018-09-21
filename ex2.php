d8c66bbdadf9e1a12b7a69215cbc4c98844cbab2
root <malvarezruiz@iesesteveterradas.cat>
2018-09-21 18:50:36 +0200

    Ejercicios 1 y 2

---
+++ ex2.php
@@ -0,0 +1,17 @@
+<?php
+$i = 12;
+$o = True;
+$u = array('Hola','adios');
+$tipus_de_i = gettype( $i );
+$tipus_de_o = gettype( $o );
+$tipus_de_u = gettype( $u );
+echo "La variable \$i 
+      conté el valor $i 
+      i és del tipus $tipus_de_i<br>";
+echo "La variable \$o
+      conté el valor $o
+      i és de tipus $tipus_de_o<br>";
+echo "La variable \$u
+      conté el valor $u 
+      i és del tipus $tipus_de_u<br>";
+?>
